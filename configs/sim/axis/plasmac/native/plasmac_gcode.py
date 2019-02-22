e94de14151fa175e1544367b91343fa232521375
Phillip Carter <phillcarter54@gmail.com>
2019-02-22 18:17:52 +1100

    plasmac: configs - fix gcode z removal filter

--- configs/sim/axis/plasmac/native/plasmac_gcode.py
+++ configs/sim/axis/plasmac/native/plasmac_gcode.py
@@ -5,39 +5,38 @@ import sys
 infile = sys.argv[1]
 f = open(infile, 'r')
 for line in f:
-    if line.startswith(';'):
+    if line.strip().startswith(';'):
         print line.rstrip()
-    elif line.startswith('('):
+    elif line.strip().startswith('('):
         print line.rstrip()
     elif not 'z' in line.lower():
         print line.rstrip()
+    elif 1 not in [c in line.lower() for c in 'xyabcuvw']:
+        print '(%s)' % (line.rstrip())
     else:
-        if not 'x' in line.lower()\
-        and not 'y' in line.lower()\
-        and not 'a' in line.lower()\
-        and not 'b' in line.lower()\
-        and not 'c' in line.lower()\
-        and not 'u' in line.lower()\
-        and not 'v' in line.lower()\
-        and not 'w' in line.lower():
-            print '(%s)' % (line.rstrip())
-            pass
-        else:
-            newline = ''
-            newz = ''
-            removing = 0
-            for bit in line:
-                if removing:
-                    if bit in '0123456789.- ':
-                        newz += bit
-                    else:
-                        removing = 0
-                        if newz:
-                            newz += ')'
-                        newline += bit
-                elif bit.lower() == 'z':
-                    removing = 1
-                    newz += '(Z'
+        newline = ''
+        newz = ''
+        removing = 0
+        comment = 0
+        for bit in line:
+            if comment:
+                if bit == ')':
+                    comment = 0
+                newline += bit
+            elif removing:
+                if bit in '0123456789.- ':
+                    newz += bit
                 else:
+                    removing = 0
+                    if newz:
+                        newz = newz.rstrip() + ')'
                     newline += bit
-            print '%s %s'% (newline.rstrip(), newz)
+            elif bit == '(':
+                comment = 1
+                newline += bit
+            elif bit.lower() == 'z':
+                removing = 1
+                newz += '(' + bit
+            else:
+                newline += bit
+        print '%s %s'% (newline.rstrip(), newz)
