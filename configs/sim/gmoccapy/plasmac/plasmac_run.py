afaa89e28c3d2d442d0cb0f61f4e4c572b9c2836
Phillip Carter <phillcarter54@gmail.com>
2019-05-05 18:06:49 +1000

    plasmac: gmoccapt - add tool handling

--- configs/sim/gmoccapy/plasmac/plasmac_run.py
+++ configs/sim/gmoccapy/plasmac/plasmac_run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-plasmac_panel.py
+plasmac_run.py
 Copyright (C) 2019  Phillip A Carter
 
 This program is free software; you can redistribute it and/or modify it
@@ -29,67 +29,46 @@ import gladevcp
 
 class HandlerClass:
 
-    def check_materials_file(self):
+    def check_tool_file(self):
         version = '[VERSION 1]'
-        tempMaterialsList = []
-        if os.path.exists(self.materialsFile):
-            if not version in open(self.materialsFile).read():
-                print '*** upgrading material file...'
-                with open(self.materialsFile, 'r') as f_in:
-                    for line in f_in:
-                        if not line.strip().startswith('#') and len(line.strip()):
-                            name, p_height, p_delay, pj_height, pj_delay, c_height, c_speed, c_amps, c_volts = line.split(',')
-                            name = name.strip()
-                            tempMaterialsList.append([name, p_height, p_delay, pj_height, pj_delay, c_height, c_speed, c_amps, c_volts])
-                with open(self.materialsFile, 'w') as f_out:
-                    f_out.write('\
-#plasmac materials file\
-\n#the next line is required for version checking\
-\n' + version + '\n\
-\n#example only, may be deleted\
-\n#[MILD STEEL 1thick]\
-\n#PIERCE_HEIGHT      = \
-\n#PIERCE_DELAY       = \
-\n#PUDDLE_JUMP_HEIGHT = (optional, set to 0 or delete if not required)\
-\n#PUDDLE_JUMP_DELAY  = (optional, set to 0 or delete if not required)\
-\n#CUT_HEIGHT         = \
-\n#CUT_SPEED          = \
-\n#CUT_AMPS           = (optional, only used for operator information)\
-\n#CUT_VOLTS          = (modes 0 & 1 only, if not using auto voltage sampling)\
-\n')
-                    for material in tempMaterialsList:
-                        f_out.write('\
-\n[' + material[0] + ']\
-\nPIERCE_HEIGHT      = ' + material[1] + '\
-\nPIERCE_DELAY       = ' + material[2] + '\
-\nPUDDLE_JUMP_HEIGHT = ' + material[3] + '\
-\nPUDDLE_JUMP_DELAY  = ' + material[4] + '\
-\nCUT_HEIGHT         = ' + material[5] + '\
-\nCUT_SPEED          = ' + material[6] + '\
-\nCUT_AMPS           = ' + material[7] + '\
-\nCUT_VOLTS          = ' + material[8] + '\
-')
-        else:
-            with open(self.materialsFile, 'w') as f_out:
-                f_out.write('\
-#plasmac material file\
-\n#the next line is required for version checking\
-\n' + version + '\n\
-\n#example only, may be deleted\
-\n#[MILD STEEL 1thick]\
-\n#PIERCE_HEIGHT      = \
-\n#PIERCE_DELAY       = \
-\n#PUDDLE_JUMP_HEIGHT = (optional, set to 0 or delete if not required)\
-\n#PUDDLE_JUMP_DELAY  = (optional, set to 0 or delete if not required)\
-\n#CUT_HEIGHT         = \
-\n#CUT_SPEED          = \
-\n#CUT_AMPS           = (optional, only used for operator information)\
-\n#CUT_VOLTS          = (modes 0 & 1 only, if not using auto voltage sampling)\
-\n')
-            print '*** new material file,', self.materialsFile, 'created'
+        tempToolDict = {}
+        if os.path.exists(self.toolFile):
+            if not version in open(self.toolFile).read():
+                print '*** upgrading tool file if we needed to ...'
+                # well, it should if we ever need it and we write the code :-)
+                #with open(self.toolFile, 'r') as f_in:
+                #    for line in f_in:
+                #        if not line.strip().startswith('#') and len(line.strip()):
+                #            read in old version
+                #            convert to new version
+                #            write new version
+        else: # create a new tool file if it doesn't exist
+            with open(self.toolFile, 'w') as f_out:
+                f_out.write(\
+                    '#plasmac tool file\n'\
+                    '#the next line is required for version checking\n'\
+                    + version + '\n\n'\
+                    '#example only, may be deleted\n'\
+                    '#[TOOL_NUMBER_1]    = \n'\
+                    '#NAME               = \n'\
+                    '#KERF_WIDTH         = \n'\
+                    '#THC                = (0 = off, 1 = on)\n'\
+                    '#PIERCE_HEIGHT      = \n'\
+                    '#PIERCE_DELAY       = \n'\
+                    '#PUDDLE_JUMP_HEIGHT = (optional, set to 0 or delete if not required)\n'\
+                    '#PUDDLE_JUMP_DELAY  = (optional, set to 0 or delete if not required)\n'\
+                    '#CUT_HEIGHT         = \n'\
+                    '#CUT_SPEED          = \n'\
+                    '#CUT_AMPS           = (optional, only used for operator information)\n'\
+                    '#CUT_VOLTS          = (modes 0 & 1 only, if not using auto voltage sampling)\n'\
+                    '\n')
+            print '*** new material file,', self.toolFile, 'created'
 
-    def get_materials(self):
-        name = 'Default'
+    def get_tools(self):
+        t_number = 0
+        t_name = 'Default'
+        k_width = self.builder.get_object('kerf-width').get_value()
+        thc_enable = self.builder.get_object('thc-enable').get_active()
         p_height = self.builder.get_object('pierce-height').get_value()
         p_delay = self.builder.get_object('pierce-delay').get_value()
         pj_height = self.builder.get_object('puddle-jump-height').get_value()
@@ -98,18 +77,23 @@ class HandlerClass:
         c_speed = self.builder.get_object('cut-feed-rate').get_value()
         c_amps = self.builder.get_object('cut-amps').get_value()
         c_volts = self.builder.get_object('cut-volts').get_value()
-        self.builder.get_object('material').set_active(0)
         try:
-            with open(self.materialsFile, 'r') as f_in:
+            with open(self.toolFile, 'r') as f_in:
                 for line in f_in:
                     if not line.startswith('#'):
-                        if line.startswith('[') and line.strip().endswith(']') and not 'VERSION' in line:
-                            self.materialsList.append([name, p_height, p_delay, pj_height, pj_delay, c_height, c_speed, c_amps, c_volts])
-                            if name != 'Default':
-                                iter = self.builder.get_object('materials').append()
-                                self.builder.get_object('materials').set(iter, 0, name, 1, p_height, 2, p_delay, 3, pj_height, 4, pj_delay, 5, c_height, 6, c_speed, 7, c_amps, 8, c_volts)
-                            name = line.strip().lstrip('[').rstrip(']')
-                            p_height = p_delay = pj_height = pj_delay = c_height = c_speed = c_amps = c_volts = 0
+                        if line.startswith('[TOOL_NUMBER_') and line.strip().endswith(']'):
+                            self.toolFileDict[t_number] = [t_name, k_width, thc_enable, p_height, p_delay, pj_height, pj_delay, c_height, c_speed, c_amps, c_volts]
+                            a,b,c = line.split('_')
+                            t_number = int(c.replace(']',''))
+                            t_name = 'none'
+                            k_width = thc_enable = p_height = p_delay = pj_height = pj_delay = c_height = c_speed = c_amps = c_volts =  0
+                        elif line.startswith('NAME'):
+                            t_name = line.split('=')[1].strip()
+                        elif line.startswith('KERF_WIDTH'):
+                            k_width = float(line.split('=')[1].strip())
+                            self.toolKerfMap[t_number] = k_width
+                        elif line.startswith('THC'):
+                            thc_enable = int(line.split('=')[1].strip())
                         elif line.startswith('PIERCE_HEIGHT'):
                             p_height = float(line.split('=')[1].strip())
                         elif line.startswith('PIERCE_DELAY'):
@@ -126,49 +110,74 @@ class HandlerClass:
                             c_amps = float(line.split('=')[1].strip())
                         elif line.startswith('CUT_VOLTS'):
                             c_volts = float(line.split('=')[1].strip())
-                self.materialsList.append([name, p_height, p_delay, pj_height, pj_delay, c_height, c_speed, c_amps, c_volts])
-                if name != 'Default':
-                    iter = self.builder.get_object('materials').append()
-                    self.builder.get_object('materials').set(iter, 0, name, 1, p_height, 2, p_delay, 3, pj_height, 4, pj_delay, 5, c_height, 6, c_speed, 7, c_amps, 8, c_volts)
+                self.toolFileDict[t_number] = [t_name, k_width, thc_enable, p_height, p_delay, pj_height, pj_delay, c_height, c_speed, c_amps, c_volts]
         except:
             print '*** materials file,', self.materialsFile, 'is invalid'
+        self.toolList = []
+        for tool in self.toolKerfMap.keys():
+            self.toolList.append(tool)
+        if sorted(self.toolFileDict,reverse=True)[0] > 99999:
+            self.builder.get_object('tool-number-adj').configure(0,0,99999,1,0,0)
+            print '*** largest tool number is 99999 ***'
+        else:
+            self.builder.get_object('tool-number-adj').configure(0,0,sorted(self.toolFileDict,reverse=True)[0],1,0,0)
 
     def on_save_clicked(self,widget,data=None):
         self.save_settings()
-        self.materialsList[0][1] = self.builder.get_object('pierce-height').get_value()
-        self.materialsList[0][2] = self.builder.get_object('pierce-delay').get_value()
-        self.materialsList[0][3] = self.builder.get_object('puddle-jump-height').get_value()
-        self.materialsList[0][4] = self.builder.get_object('puddle-jump-delay').get_value()
-        self.materialsList[0][5] = self.builder.get_object('cut-height').get_value()
-        self.materialsList[0][6] = self.builder.get_object('cut-feed-rate').get_value()
-        self.materialsList[0][7] = self.builder.get_object('cut-amps').get_value()
-        self.materialsList[0][8] = self.builder.get_object('cut-volts').get_value()
-        self.builder.get_object('material').set_active(0)
+        self.toolFileDict[0][0] = self.builder.get_object('tool-name').get_text()
+        self.toolFileDict[0][1] = self.builder.get_object('kerf-width').get_value()
+        self.toolFileDict[0][2] = self.builder.get_object('thc-enable').get_active()
+        self.toolFileDict[0][3] = self.builder.get_object('pierce-height').get_value()
+        self.toolFileDict[0][4] = self.builder.get_object('pierce-delay').get_value()
+        self.toolFileDict[0][5] = self.builder.get_object('puddle-jump-height').get_value()
+        self.toolFileDict[0][6] = self.builder.get_object('puddle-jump-delay').get_value()
+        self.toolFileDict[0][7] = self.builder.get_object('cut-height').get_value()
+        self.toolFileDict[0][8] = self.builder.get_object('cut-feed-rate').get_value()
+        self.toolFileDict[0][9] = self.builder.get_object('cut-amps').get_value()
+        self.toolFileDict[0][10] = self.builder.get_object('cut-volts').get_value()
 
     def on_reload_clicked(self,widget,data=None):
-        self.materialsUpdate = True
+        self.toolUpdate = True
         self.load_settings()
-        self.materialsList = []
-        for row in range(len(self.builder.get_object('materials')) - 1, 0, -1):
-            self.builder.get_object('materials').remove(self.builder.get_object('materials')[row].iter) 
-        self.get_materials()
-        self.materialsUpdate = False
+        self.toolFileDict = {}
+        self.toolKerfMap = {0: 0.0}
+        self.get_tools()
+        self.toolUpdate = False
 
-    def on_material_changed(self,widget,data=None):
-        if not self.materialsUpdate:
-            material = self.builder.get_object('material').get_active()
-            self.builder.get_object('pierce-height').set_value(self.materialsList[material][1])
-            self.builder.get_object('pierce-delay').set_value(self.materialsList[material][2])
-            self.builder.get_object('puddle-jump-height').set_value(self.materialsList[material][3])
-            self.builder.get_object('puddle-jump-delay').set_value(self.materialsList[material][4])
-            self.builder.get_object('cut-height').set_value(self.materialsList[material][5])
-            self.builder.get_object('cut-feed-rate').set_value(self.materialsList[material][6])
-            self.builder.get_object('cut-amps').set_value(self.materialsList[material][7])
-            self.builder.get_object('cut-volts').set_value(self.materialsList[material][8])
+    def on_tool_number_value_changed(self,widget):
+        tool = int(self.builder.get_object('tool-number').get_value())
+        if tool in self.toolKerfMap.keys():
+            self.builder.get_object('tool-name').set_text(self.toolFileDict[tool][0])
+            self.builder.get_object('kerf-width').set_value(self.toolFileDict[tool][1])
+            self.builder.get_object('thc-enable').set_active(self.toolFileDict[tool][2])
+            self.builder.get_object('pierce-height').set_value(self.toolFileDict[tool][3])
+            self.builder.get_object('pierce-delay').set_value(self.toolFileDict[tool][4])
+            self.builder.get_object('puddle-jump-height').set_value(self.toolFileDict[tool][5])
+            self.builder.get_object('puddle-jump-delay').set_value(self.toolFileDict[tool][6])
+            self.builder.get_object('cut-height').set_value(self.toolFileDict[tool][7])
+            self.builder.get_object('cut-feed-rate').set_value(self.toolFileDict[tool][8])
+            self.builder.get_object('cut-amps').set_value(self.toolFileDict[tool][9])
+            self.builder.get_object('cut-volts').set_value(self.toolFileDict[tool][10])
+            hal.set_p('plasmac_run.tool-change-number',str(tool))
+        else:
+            if tool < self.oldTool:
+                self.builder.get_object('tool-number').set_value(self.toolList[self.toolList.index(self.oldTool) - 1])
+            else:
+                self.builder.get_object('tool-number').set_value(self.toolList[self.toolList.index(self.oldTool) + 1])
+        self.oldTool = tool
+
+    def tool_change_number_changed(self,halpin):
+        tool = halpin.get()
+        if tool in self.toolKerfMap.keys():
+            self.builder.get_object('tool-number').set_value(tool)
+            hal.set_p('plasmac_run.tool-change-diameter',str(self.toolKerfMap[tool]))
+        else:
+            hal.set_p('plasmac_run.tool-change-diameter','-2')
+
 
     def on_setupFeedRate_value_changed(self, widget):
         self.builder.get_object('probe-feed-rate-adj').configure(self.builder.get_object('probe-feed-rate').get_value(),0,self.builder.get_object('setup-feed-rate').get_value(),1,0,0)
-        
+
     def configure_widgets(self):
         self.builder.get_object('cornerlock-enable').set_active(1)
         self.builder.get_object('cornerlock-threshold').set_digits(0)
@@ -191,8 +200,12 @@ class HandlerClass:
         self.builder.get_object('thc-enable').set_active(1)
         self.builder.get_object('thc-threshold').set_digits(2)
         self.builder.get_object('thc-threshold-adj').configure(1,0.05,9,0.01,0,0)
+        self.builder.get_object('tool-number').set_digits(0)
+        self.builder.get_object('tool-number-adj').configure(0,0,99999,1,0,0)
         self.builder.get_object('use-auto-volts').set_active(1)
         if self.i.find('TRAJ', 'LINEAR_UNITS').lower() == 'mm':
+            self.builder.get_object('kerf-width').set_digits(2)
+            self.builder.get_object('kerf-width-adj').configure(0.5,0,5,0.01,0,0)
             self.builder.get_object('cut-feed-rate').set_digits(0)
             self.builder.get_object('cut-feed-rate-adj').configure(4000,0,9999,1,0,0)
             self.builder.get_object('cut-height').set_digits(2)
@@ -200,6 +213,8 @@ class HandlerClass:
             self.builder.get_object('pierce-height').set_digits(2)
             self.builder.get_object('pierce-height-adj').configure(4,0,25,0.01,0,0)
         elif self.i.find('TRAJ', 'LINEAR_UNITS').lower() == 'inch':
+            self.builder.get_object('kerf-width').set_digits(3)
+            self.builder.get_object('kerf-width-adj').configure(0.02,0,0.25,1,0,0)
             self.builder.get_object('cut-feed-rate').set_digits(1)
             self.builder.get_object('cut-feed-rate-adj').configure(160,0,400,0.1,0,0)
             self.builder.get_object('cut-height').set_digits(3)
@@ -290,7 +305,9 @@ class HandlerClass:
             except:
                 print '*** plasmac configuration file,', self.configFile, 'is invalid ***'
             for item in self.configDict:
-                if isinstance(self.builder.get_object(item), gladevcp.hal_widgets.HAL_SpinButton):
+                if item == 'tool-number' or item == 'kerf-width':
+                    self.builder.get_object(item).set_value(0)
+                elif isinstance(self.builder.get_object(item), gladevcp.hal_widgets.HAL_SpinButton):
                     if item in tmpDict:
                         self.builder.get_object(item).set_value(float(self.configDict.get(item)))
                     else:
@@ -319,9 +336,10 @@ class HandlerClass:
         try:
             with open(self.configFile, 'w') as f_out:
                 f_out.write('#plasmac configuration file, format is:\n#name = value\n\n')
-                #for key in self.configDict:
                 for key in sorted(self.configDict.iterkeys()):
-                    if isinstance(self.builder.get_object(key), gladevcp.hal_widgets.HAL_SpinButton):
+                    if key == 'tool-number' or key == 'kerf-width':
+                        pass
+                    elif isinstance(self.builder.get_object(key), gladevcp.hal_widgets.HAL_SpinButton):
                         value = self.builder.get_object(key).get_value()
                         f_out.write(key + '=' + str(value) + '\n')
                     elif isinstance(self.builder.get_object(key), gladevcp.hal_widgets.HAL_CheckButton):
@@ -337,21 +355,29 @@ class HandlerClass:
         self.halcomp = halcomp
         self.builder = builder
         self.i = linuxcnc.ini(os.environ['INI_FILE_NAME'])
+        self.c = linuxcnc.command()
+        self.s = linuxcnc.stat()
+        self.toolNumberPin = hal_glib.GPin(halcomp.newpin('tool-change-number', hal.HAL_S32, hal.HAL_IN))
+        self.toolNumberPin.connect('value-changed', self.tool_change_number_changed)
+        self.toolDiameterPin = hal_glib.GPin(halcomp.newpin('tool-change-diameter', hal.HAL_FLOAT, hal.HAL_IN))
         self.thcFeedRate = (float(self.i.find('AXIS_Z', 'MAX_VELOCITY')) * \
                               float(self.i.find('AXIS_Z', 'OFFSET_AV_RATIO'))) * 60
         hal.set_p('plasmac.thc-feed-rate','%f' % (self.thcFeedRate))
         self.configFile = self.i.find('EMC', 'MACHINE').lower() + '_run.cfg'
         self.prefFile = self.i.find('EMC', 'MACHINE') + '.pref'
-        self.materialsFile = self.i.find('EMC', 'MACHINE').lower() + '.mat'
-        self.materialsList = []
+        self.toolFile = self.i.find('EMC', 'MACHINE').lower() + '_tool.tbl'
+        self.toolFileDict = {}
+        self.toolKerfMap = {0: 0.0}
+        self.toolDict = {}
         self.configDict = {}
         hal.set_p('plasmac.mode','%d' % (int(self.i.find('PLASMAC','MODE') or '0')))
         self.oldMode = 9
-        self.materialsUpdate = False
+        self.oldTool = -1
+        self.toolUpdate = False
         self.configure_widgets()
-        self.check_materials_file()
-        self.get_materials()
         self.load_settings()
+        self.check_tool_file()
+        self.get_tools()
         self.set_theme()
         gobject.timeout_add(100, self.periodic)
 
--- configs/sim/gmoccapy/plasmac/python/remap.py
+++ configs/sim/gmoccapy/plasmac/python/remap.py
@@ -3,7 +3,7 @@ from interpreter import *
 import hal
 throw_exceptions = 1
 
-# REMAP = F prolog=setfeed_prolog  ngc=setfeed epilog=setfeed_epilog
+# REMAP = F prolog=plasmac_feed_prolog ngc=plasmac_feed epilog=plasmac_feed_epilog
 # exposed parameter: #<feed>
 def plasmac_feed_prolog(self,**words):
     try:
@@ -38,4 +38,49 @@ def plasmac_feed_epilog(self,**words):
         return INTERP_ERROR
     return INTERP_OK
 
+# REMAP = T prolog=plasmac_tool_prolog ngc=plasmac_tool epilog=plasmac_tool_epilog
+# exposed parameters: #<tool> #<pocket>
+def plasmac_tool_prolog(self,**words):
+    import time
+    try:
+        cblock = self.blocks[self.remap_level]
+        if not cblock.t_flag:
+            self.set_errormsg("T requires a value")
+            return INTERP_ERROR
+        tool  = cblock.t_number
+        if tool >= 0 and tool != hal.get_value('plasmac_run.tool-change-number'):
+            hal.set_p('plasmac_run.tool-change-diameter','-1')
+            hal.set_p('plasmac_run.tool-change-number', str(tool))
+            start = time.time()
+            # wait up to to 3 seconds for tool-change-diameter signal from plasmac
+            while hal.get_value('plasmac_run.tool-change-diameter') == -1:
+                if time.time() > start + 3:
+                    self.set_errormsg("T%d: timeout waiting for tool-change-diameter value" % (tool))
+                    return INTERP_ERROR
+            if hal.get_value('plasmac_run.tool-change-diameter') >= 0:
+                tool = 0
+            else:
+                self.set_errormsg("Tool #%d: does not exist" % (hal.get_value('plasmac_run.tool-change-number'))) # non existant tool
+                return INTERP_ERROR
+        else:
+            tool = 0
+        self.params["_kerf_width"] = hal.get_value('plasmac_run.tool-change-diameter')
+        self.params["tool"] = tool
+        self.params["pocket"] = tool
+        return INTERP_OK
+    except Exception, e:
+        self.set_errormsg("T%d/prepare_prolog: %s" % (int(words['t']), e))
+        return INTERP_ERROR
 
+def plasmac_tool_epilog(self, **words):
+    try:
+        if self.return_value > 0:
+            self.selected_tool = int(self.params["tool"])
+            self.selected_pocket = int(self.params["pocket"])
+            emccanon.SELECT_POCKET(self.selected_pocket, self.selected_tool)
+            return INTERP_OK
+        else:
+            return "T%d: aborted (return code %.1f)" % (int(self.params["tool"]),self.return_value)
+
+    except Exception, e:
+        return "T%d/prepare_epilog: %s" % (tool,e)
